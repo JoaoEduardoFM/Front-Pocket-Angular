a4f82d884f4e3371748eda8823e6c06a28c1d0f7
João Martins <90796699+JoaoEduardoFM@users.noreply.github.com>
2024-09-03 06:01:20 -0300

    correcao de bugs  ao vender produto

--- estudo-angular/src/app/produto/produto-caixa/produto-caixa.component.ts
+++ estudo-angular/src/app/produto/produto-caixa/produto-caixa.component.ts
@@ -18,34 +18,16 @@ export class ProdutoCaixaComponent implements OnInit {
   produtos: Produto[] = [];
   cart: CartItem[] = [];
   total: number = 0;
-  searchTerm: string = '';
-  nome: String = '';
-  login: string = '';
-  cpf: string = '';
-  id: number = 0;
-  pageSize: number = 5;
-  pagina: number = 1;
+  nome: string = '';
 
   constructor(
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
     private produtoService: ProdutoService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.carregarPagina({ first: 0, rows: this.pageSize });
-  }
-
-  carregarProdutos() {
-    this.produtoService.getProdutoListPage(0).subscribe(
-      data => {
-        this.produtos = data.content;
-      },
-      error => {
-        console.log('Ocorreu um erro ao buscar os produtos:', error);
-        this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Falha ao carregar produtos.' });
-      }
-    );
+    this.carregarPagina();
   }
 
   addToCart(produto: Produto) {
@@ -64,8 +46,7 @@ export class ProdutoCaixaComponent implements OnInit {
     this.updateTotal();
   }
 
-
-  removeFromCart(item: Produto) {
+  removeFromCart(item: CartItem) {
     const index = this.cart.findIndex(cartItem => cartItem.id === item.id);
     if (index > -1) {
       if (this.cart[index].cartQuantity > 1) {
@@ -75,9 +56,6 @@ export class ProdutoCaixaComponent implements OnInit {
       }
       this.updateTotal();
     }
-  }
-  atualizarGrid() {
-    this.carregarPagina({ first: this.pageSize * (this.pagina - 1), rows: this.pageSize });
   }
 
   updateTotal() {
@@ -95,7 +73,6 @@ export class ProdutoCaixaComponent implements OnInit {
           this.produtoService.saveProduto(updatedProduct).subscribe(
             response => {
               console.log('Produto atualizado:', response);
-              this.carregarPagina({ first: 0, rows: this.pageSize });
             },
             error => {
               console.error('Erro ao atualizar produto:', error);
@@ -106,45 +83,40 @@ export class ProdutoCaixaComponent implements OnInit {
         this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Venda realizada com sucesso!' });
         this.cart = [];
         this.total = 0;
-        this.carregarProdutos();
+        this.carregarPagina();
       }
     });
   }
 
   aplicarFiltros() {
     if (this.nome) {
-      this.consutarNome();
+      this.consultarNome();
     } else {
-      this.carregarPagina({ first: 0, rows: this.pageSize });
+      this.carregarPagina();
     }
   }
 
   limparCampos() {
     this.nome = '';
-    this.carregarPagina({ first: 0, rows: this.pageSize });
   }
 
-  consutarNome() {
+  consultarNome() {
     this.produtoService.getNome(this.nome).subscribe(data => {
       this.produtos = data;
       this.total = data.length;
     });
   }
 
-  carregarPagina(event: TableLazyLoadEvent) {
-    const rows = event.rows ?? this.pageSize;
-    const pageNumber = (event.first! / rows) + 1;
-    this.pageSize = rows;
-
-    this.produtoService.getProdutoListPage(pageNumber - 1).subscribe(
-      data => {
-        this.pagina = pageNumber;
-        this.produtos = data.content;
-        this.total = data.totalElements;
+  carregarPagina() {
+    this.produtoService.getProdutos().subscribe(
+      (data: Produto[]) => {
+        this.produtos = data; // Ajuste se a resposta for um array de produtos
+        this.total = data.length; // Ajuste se `total` é simplesmente o comprimento do array
       },
       error => {
-        console.log('Ocorreu um erro ao buscar os usuários:', error);
+        console.error('Ocorreu um erro ao buscar os produtos:', error);
       }
     );
   }
+  
 }
